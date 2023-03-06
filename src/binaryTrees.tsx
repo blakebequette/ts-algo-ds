15bf0ff7dbf15851414505a7450bf7ca4f8db201
beq <blakebequette@gmail.com>
2023-03-05 20:53:49 -0800

    add DFS / BFS

--- src/binaryTrees.tsx
+++ src/binaryTrees.tsx
@@ -1,8 +1,30 @@
 type BinaryNode = {
     val: number,
-    left: BinaryNode,
-    right: BinaryNode,
+    left: BinaryNode | null ,
+    right: BinaryNode | null,
 }
 
 
-function dfs()
+function dfs(node: BinaryNode){
+    // pre-order: process node.val
+    if (node.left) {dfs(node.left)}
+    if (node.right) {dfs(node.right)}
+    // post order: process node val
+    return
+}
+
+function breadthFS(node: BinaryNode){
+    if (!node) return node
+    let queue = [node]
+    while (queue.length){
+        // process current node 
+        const currentNode = queue.shift()
+        if (currentNode?.left) {
+            queue.push(currentNode.left)
+        } 
+        if (currentNode?.right){
+            queue.push(currentNode.right)
+        }
+    }
+    return 
+}
--- target/binaryTrees.js
+++ target/binaryTrees.js
@@ -1,1 +1,28 @@
 "use strict";
+function dfs(node) {
+    // pre-order: process node.val
+    if (node.left) {
+        dfs(node.left);
+    }
+    if (node.right) {
+        dfs(node.right);
+    }
+    // post order: process node val
+    return;
+}
+function breadthFS(node) {
+    if (!node)
+        return node;
+    let queue = [node];
+    while (queue.length) {
+        // process current node 
+        const currentNode = queue.shift();
+        if (currentNode === null || currentNode === void 0 ? void 0 : currentNode.left) {
+            queue.push(currentNode.left);
+        }
+        if (currentNode === null || currentNode === void 0 ? void 0 : currentNode.right) {
+            queue.push(currentNode.right);
+        }
+    }
+    return;
+}
